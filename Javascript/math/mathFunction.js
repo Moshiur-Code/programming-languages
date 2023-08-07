d818e9e469038308681584aa4fda0906ec0a231d
Moshiur <moshiurrahman4195@gmail.com>
2023-08-07 11:19:08 +0600

    math function added

---
+++ Javascript/math/mathFunction.js
@@ -0,0 +1,35 @@
+// Math function
+
+// math pow 
+console.log('Power function:',  Math.pow(2, 3));
+
+// math abs
+let a = 25, b = 40;
+let result = a - b;
+
+console.log('result:', result, '\n');
+
+// use of abs()
+let realValue = Math.abs(result);
+
+if (realValue < 5) {
+    console.log('permission granted \n');
+}
+else {
+    console.log('permission not granted \n');
+}
+
+
+// use of round()
+console.log('Use of math round fuction \n');
+const number1 = 2.4999;
+const number2 = 2.5000;
+
+console.log(Math.round(number1));
+console.log(Math.round(number2));
+
+
+// use of ceil() and floor()
+
+
+// use of random()
